164f765fd925787e08982edc3423e3baa87ae4d6
Avinash Pokhrel <106798140+xetri@users.noreply.github.com>
2025-09-22 13:31:48 +0000

    Switch to plain html

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+
+export default defineConfig({
+	plugins: [
+		injectHTML({
+			tagName: "load",
+			sourceAttr: "src",
+		})
+	],
+});
